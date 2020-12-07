6cca04419005b7b8cbfa93143f83a23111d0a2e6
AlekseyChi <chiganov.aleksey@mail.ru>
2020-12-07 22:13:11 +0300

    Shell_sort

---
+++ shellsort_by_Chiganov.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> shellsort(vector<int> aVector)
+{
+    int end = aVector.size() - 1;
+    int h;
+    for (h = 1; h <= end / 9; h = 3 * h + 1);
+
+    for (; h > 0; h /= 3)
+        for (auto i = h; i <= end; i++)
+        {
+            int j = i;
+            int v = aVector[i];
+            while (j >= h && v < aVector[j - h])
+            {
+                aVector[j] = aVector[j - h];
+                j -= h;
+            }
+            aVector[j] = v;
+        }
+    return aVector;
+}
+
+vector<double> shellsort(vector<double> aVector)
+{
+    //return unsorted vector if data doesn't match your method
+    return aVector;
+}
